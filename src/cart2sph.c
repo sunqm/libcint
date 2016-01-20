90ad28a141b55b21e0379fa06f0f93b0aae97b16
Qiming Sun <osirpt.sun@gmail.com>
2016-01-19 19:31:25 -0500

    Fix function CINTaddressxxx return type

--- src/cart2sph.c
+++ src/cart2sph.c
@@ -2813,17 +2813,17 @@ static struct cart2sp_t g_c2s[] = {
         {g_trans_cart2sph+6370,NULL,NULL},
 };
 
-double *CINTaddress_cart2sph(FINT l)
+const double *CINTaddress_cart2sph(FINT l)
 {
         return g_c2s[l].cart2sph;
 }
 
-double *CINTaddress_cart2j_lt_l(FINT l)
+const double *CINTaddress_cart2j_lt_l(FINT l)
 {
         return g_c2s[l].cart2j_lt_l;
 }
 
-double *CINTaddress_cart2j_gt_l(FINT l)
+const double *CINTaddress_cart2j_gt_l(FINT l)
 {
         return g_c2s[l].cart2j_gt_l;
 }
