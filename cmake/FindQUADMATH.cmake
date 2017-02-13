1cc9f81e00d4b963d1775d9dd8a8789b708eab7e
Qiming Sun <osirpt.sun@gmail.com>
2017-02-13 15:42:44 -0800

    Fix #19  and fix quadmath search

--- cmake/FindQUADMATH.cmake
+++ cmake/FindQUADMATH.cmake
@@ -1,20 +1,11 @@
-find_path(QUADMATH_INCLUDE_DIR
-  NAMES quadmath.h
-  PATHS
-  ${QUADMATH_ROOT_DIR}/include
-  ${QUADMATH_ROOT_DIR}/usr/include
-  ${QUADMATH_ROOT_DIR}/usr/local/include
-)
-
 find_library(QUADMATH_LIBRARY
   NAMES quadmath
   PATHS 
+  /usr/lib /usr/lib64
+  /usr/local/lib /usr/local/lib64
+  /usr/x86_64-linux-gnu/*
+  /usr/lib/gcc/x86_64-linux-gnu/*
   ${QUADMATH_ROOT_DIR}
-  ${QUADMATH_ROOT_DIR}/usr/lib
-  ${QUADMATH_ROOT_DIR}/usr/lib64
-  ${QUADMATH_ROOT_DIR}/usr/local/lib
-  ${QUADMATH_ROOT_DIR}/lib
-  ${QUADMATH_ROOT_DIR}/lib64
 )
 
 include(FindPackageHandleStandardArgs) 
@@ -22,7 +13,6 @@ find_package_handle_standard_args(QUADMATH
   FOUND_VAR
   QUADMATH_FOUND
   REQUIRED_VARS
-  QUADMATH_INCLUDE_DIR
   QUADMATH_LIBRARY
 )
 
