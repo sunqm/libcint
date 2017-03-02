52739b7ad814086dd324459a7d76300d89fedf19
Qiming Sun <osirpt.sun@gmail.com>
2017-03-02 13:57:16 -0800

    Update quadmath detection code

--- cmake/FindQUADMATH.cmake
+++ cmake/FindQUADMATH.cmake
@@ -1,19 +1,15 @@
-find_library(QUADMATH_LIBRARY
-  NAMES quadmath
-  PATHS 
-  /usr/lib /usr/lib64
-  /usr/local/lib /usr/local/lib64
-  /usr/x86_64-linux-gnu/*
-  /usr/lib/gcc/x86_64-linux-gnu/*
-  ${QUADMATH_ROOT_DIR}
-)
+set(QUADMATH_TEST_SOURCE
+"
+#include <quadmath.h>
+int main() {
+fabsq(1);
+}
+")
 
-include(FindPackageHandleStandardArgs) 
-find_package_handle_standard_args(QUADMATH
-  FOUND_VAR
-  QUADMATH_FOUND
-  REQUIRED_VARS
-  QUADMATH_LIBRARY
-)
+include(CheckCSourceCompiles)
+set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+set(CMAKE_REQUIRED_LIBRARIES "quadmath")
+set(CHECK_QUADMATH_ADD_LIBRARIES LINK_LIBRARIES quadmath)
+check_c_source_compiles("${QUADMATH_TEST_SOURCE}" QUADMATH_FOUND)
+set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
 
-mark_as_advanced(QUADMATH_INCLUDE_DIR, QUADMATH_LIBRARY)
