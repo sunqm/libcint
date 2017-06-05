97bda8c436a3283e0d38284bed9b13df4990f84b
Qiming Sun <osirpt.sun@gmail.com>
2017-06-04 23:34:02 -0700

    Update head of stg_roots.c

--- src/stg_roots.c
+++ src/stg_roots.c
@@ -1,26 +1,5 @@
-//
-// BAGEL - Parallel electron correlation program.
-// Copyright (C) 2013 Toru Shiozaki
-//
-// Author: Toru Shiozaki <shiozaki@northwestern.edu>
-// Maintainer: Shiozaki group
-//
-// This file is part of the BAGEL package.
-//
-// The BAGEL package is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Library General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
-//
-// The BAGEL package is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with the BAGEL package; see COPYING.  If not, write to
-// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-//
+// This function is ported from libslater library
+// https://github.com/nubakery/libslater
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -445,7 +424,7 @@ void CINTstg_roots(int nroots, double ta, double ua, double* rr, double* ww)
   tt = tt - it;
   tt = 2.0 * tt - 1.0;
 
-  iu = uu + 7; // 0 <= iu <= 9
+  iu = (uu + 7); // 0 <= iu <= 9
   if (iu < 0 || iu > 10) {
       fprintf(stderr, "current implementation assumes 1.0e-7 < U < 1.0e3");
       exit(1);
