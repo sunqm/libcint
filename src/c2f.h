6612db12ad4bf53db36323d31579fb4b25e52f84
Qiming Sun <osirpt.sun@gmail.com>
2018-02-14 13:05:26 -0800

    Fix Fortran APIs

--- src/c2f.h
+++ src/c2f.h
@@ -7,30 +7,34 @@
 
 #define ALL_CINT_FORTRAN_(NAME) \
 int c##NAME##_sph_(double *out, int *shls, int *atm, int *natm, \
-                    int *bas, int *nbas, double *env, CINTOpt *opt) { \
+                    int *bas, int *nbas, double *env, int64_t optptr_as_integer8) { \
+        CINTOpt **opt = (CINTOpt **)optptr_as_integer8; \
         return NAME##_sph(out, NULL, shls, \
-                          atm, *natm, bas, *nbas, env, opt, NULL); \
+                          atm, *natm, bas, *nbas, env, *opt, NULL); \
 } \
 void c##NAME##_sph_optimizer_(CINTOpt **opt, int *atm, int *natm, \
                               int *bas, int *nbas, double *env) { \
         NAME##_optimizer(opt, atm, *natm, bas, *nbas, env); \
 } \
 int c##NAME##_cart_(double *out, int *shls, int *atm, int *natm, \
-                    int *bas, int *nbas, double *env, CINTOpt *opt) { \
+                    int *bas, int *nbas, double *env, int64_t optptr_as_integer8) { \
+        CINTOpt **opt = (CINTOpt **)optptr_as_integer8; \
         return NAME##_cart(out, NULL, shls, \
-                           atm, *natm, bas, *nbas, env, opt, NULL); \
+                           atm, *natm, bas, *nbas, env, *opt, NULL); \
 } \
 void c##NAME##_cart_optimizer_(CINTOpt **opt, int *atm, int *natm, \
                                int *bas, int *nbas, double *env) { \
         NAME##_optimizer(opt, atm, *natm, bas, *nbas, env); \
 } \
 int c##NAME##_(double *out, int *shls, int *atm, int *natm, \
-               int *bas, int *nbas, double *env, CINTOpt *opt) { \
+               int *bas, int *nbas, double *env, int64_t optptr_as_integer8) { \
+        CINTOpt **opt = (CINTOpt **)optptr_as_integer8; \
         return NAME##_spinor((double complex *)out, NULL, shls, \
-                             atm, *natm, bas, *nbas, env, opt, NULL); \
+                             atm, *natm, bas, *nbas, env, *opt, NULL); \
 } \
-void c##NAME##_optimizer_(CINTOpt **opt, int *atm, int *natm, \
+void c##NAME##_optimizer_(int64_t optptr_as_integer8, int *atm, int *natm, \
                          int *bas, int *nbas, double *env) { \
+        CINTOpt **opt = (CINTOpt **)optptr_as_integer8; \
         NAME##_optimizer(opt, atm, *natm, bas, *nbas, env); \
 }
 
