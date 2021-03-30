9dfc44cc307d9bbb4f1f94df801331414ae6d751
Qiming Sun <osirpt.sun@gmail.com>
2021-03-29 20:15:50 -0700

    Update python example

--- examples/python_call.py
+++ examples/python_call.py
@@ -43,38 +43,49 @@ env.extend([0, 0, -0.8])
 ptr_env += 3
 atm.append([1,        ptr_env,  0, 0, 0, 0])
 env.extend([0, 0,  0.8])
+ptr_env += 3
 
 # basis for atom #0
 # 3s -> 2s
 env.extend([6., 2., .8])
-env.extend([.7, .6, .5, .4, .3, .2])
+env.append(.7 * gto_norm(0, 6.))
+env.append(.6 * gto_norm(0, 2.))
+env.append(.5 * gto_norm(0, .8))
+env.append(.4 * gto_norm(0, 6.))
+env.append(.3 * gto_norm(0, 2.))
+env.append(.2 * gto_norm(0, .8))
 #           ATOM_OF, ANG_OF, NPRIM_OF, NCTR_OF, KAPPA_OF, PTR_EXP, PTR_COEFF
 bas.append([0,       0,      3,        2,       0,        ptr_env, ptr_env+3, 0])
 ptr_env += 9
 env.extend([.9])
-env.extend([1.])
+env.append(1. * gto_norm(1, 0.9))
 bas.append([0,       1,      1,        1,       0,        ptr_env, ptr_env+1, 0])
-ptr_env += 1
+ptr_env += 2
 
 # basis functions for atom #1, they are the same to thoes of atom #0
 bas.extend(bas[-2:])
 
 # note the integer type
-atm = numpy.array(atm,dtype=int32)
-bas = numpy.array(bas,dtype=int32)
-env = numpy.array(env)
+atm = numpy.array(atm, dtype=numpy.intc)
+bas = numpy.array(bas, dtype=numpy.intc)
+env = numpy.array(env, dtype=numpy.double)
 
 _cint = ctypes.cdll.LoadLibrary('/path/to/libcint.so')
-c_natm = ctypes.c_int(atm.size)
-c_nbas = ctypes.c_int(bas.size)
 
 _cint.CINTcgto_spheric.restype = ctypes.c_int
-di = _cint.CINTcgto_spheric(ctypes.c_int(0), bas.ctype.data)
-dj = _cint.CINTcgto_spheric(ctypes.c_int(1), bas.ctype.data)
+_cint.CINTcgto_spheric.argtypes = [ctypes.c_int, numpy.ctypeslib.ndpointer(dtype=numpy.intc, ndim=2)]
+di = _cint.CINTcgto_spheric(0, bas)
+dj = _cint.CINTcgto_spheric(1, bas)
 
-c_shls = (ctypes.c_int * 2)(0, 1)
-buf = numpy.empty((di.value, dj.value, 3))
-_cint.cint1e_ipnuc_sph(buf.ctypes.data, c_shls, \
-                       atm.ctypes.data, c_natm, \
-                       bas.ctypes.data, c_nbas,
-                       env.ctypes.data)
+_cint.cint1e_ipnuc_sph.argtypes = [
+    numpy.ctypeslib.ndpointer(dtype=numpy.double, ndim=3),
+    (ctypes.c_int * 2),
+    numpy.ctypeslib.ndpointer(dtype=numpy.intc, ndim=2),
+    ctypes.c_int,
+    numpy.ctypeslib.ndpointer(dtype=numpy.intc, ndim=2),
+    ctypes.c_int,
+    numpy.ctypeslib.ndpointer(dtype=numpy.double, ndim=1)
+]
+buf = numpy.empty((di, dj, 3), order='F')
+_cint.cint1e_ipnuc_sph(buf, (ctypes.c_int * 2)(0, 1), atm, atm.shape[0], bas, bas.shape[1], env)
+print(buf)
